edee27cc49221bbd1506031f6a4f0ba45a581766
mariosxixis1980-boop <mariosxixis1980@gmail.com>
2026-01-28 21:54:20 +0200

    Add files via upload

---
+++ supabase.js
@@ -0,0 +1,6 @@
+// supabase.js
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
+
+export const supabase = createClient(
+  "https://nljrwrmhlrpdgeougpk.supabase.co",
+  sb_publishable_I0PqYv3fuLS8cE8hxftelA_U1_huEzN
