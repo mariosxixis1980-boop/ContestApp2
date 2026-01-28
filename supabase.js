dde34d8779c01e74b75c2d932ad80260c6cf104c
mariosxixis1980-boop <mariosxixis1980@gmail.com>
2026-01-28 16:16:33 +0200

    Update Supabase client configuration
    Updated Supabase client initialization with a new URL and public key.

--- supabase.js
+++ supabase.js
@@ -2,6 +2,6 @@
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
 export const supabase = createClient(
-  "https://nljrwvmhlrpdgeougpk.supabase.co",
-  "PASTE_YOUR_ANON_PUBLIC_KEY_HERE"
+  https://nljrwvrmhlrpdgeougpk.supabase.co
+eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5sanJ3dnJtaGxycGRnZW91Z3BrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk1OTM2OTgsImV4cCI6MjA4NTE2OTY5OH0.OtuXKuMWf92s7pTsI68YKbGVUWiQQJa-KrKgXSkyCao
 );
