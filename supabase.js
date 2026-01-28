ea7662774f6224dded04cddceada47efaeb6eb77
mariosxixis1980-boop <mariosxixis1980@gmail.com>
2026-01-28 16:13:14 +0200

    Create supabase.js

---
+++ supabase.js
@@ -0,0 +1,7 @@
+// supabase.js
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
+
+export const supabase = createClient(
+  "https://nljrwvmhlrpdgeougpk.supabase.co",
+  "PASTE_YOUR_ANON_PUBLIC_KEY_HERE"
+);
