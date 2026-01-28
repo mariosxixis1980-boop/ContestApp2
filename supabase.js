67231236c666d468407055b4931a1d94a9f0afe2
mariosxixis1980-boop <mariosxixis1980@gmail.com>
2026-01-28 16:17:57 +0200

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -2,6 +2,6 @@
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
 export const supabase = createClient(
-  https://nljrwvrmhlrpdgeougpk.supabase.co
-eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5sanJ3dnJtaGxycGRnZW91Z3BrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk1OTM2OTgsImV4cCI6MjA4NTE2OTY5OH0.OtuXKuMWf92s7pTsI68YKbGVUWiQQJa-KrKgXSkyCao
+  "https://nljrwrmhlrpdgeougpk.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5sanJ3cm1obHJwZGdlb3VncGsiLCJyb2xlIjoiYW5vbiIsImlhdCI6MTY4NTE2OTY5MCwiZXhwIjoxOTg4NzM2OTY5fQ.0tuXKuM..."
 );
