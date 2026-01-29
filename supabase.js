a4530a1b6749d6d08b15c8ed6c4310a2b9c26acb
mariosxixis1980-boop <mariosxixis1980@gmail.com>
2026-01-29 02:04:59 +0200

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,10 +1,7 @@
 // supabase.js
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
-const SUPABASE_URL = "https://nljwrmhlrpdeqougpk.supabase.co";
-const SUPABASE_ANON_KEY = "sb_publishable_oPFyZv4OW9n75NqORuJ8Gw_QQkY7NZe";
+const SUPABASE_URL = "https://nljwrmhlrpdgeougqpk.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_I0PqYv3fuLS8cE8hxftelA_U1_huEzN";
 
-export const supabase = createClient(
-SUPABASE_URL,
-SUPABASE_ANON_KEY
-);
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
